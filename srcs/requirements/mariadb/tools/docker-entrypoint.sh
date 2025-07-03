406dad2cb86795498c376ae9a9d272c39000644c
eutrius <eutrius@gmail.com>
2025-07-03 21:43:30 +0200

    chore: prevent mariadb initialization when it already exist

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+INIT_MARKER="/var/lib/mysql/.mariadb_initialized"
+
+if [ ! -f "$INIT_MARKER" ]; then
 export MARIA_PASSWORD=$(cat /run/secrets/maria_password)
 export MARIA_ROOT_PASSWORD=$(cat /run/secrets/maria_root_password)
 export MARIA_IP="${WP_HOST%%:*}"
@@ -13,23 +16,22 @@ until nc -z $MARIA_IP $MARIA_PORT; do
     sleep 1
 done
 
-echo "set root password and use native password plugin"
+echo "set root password and auth plugin"
 mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIA_ROOT_PASSWORD}'); FLUSH PRIVILEGES;"
 
-echo "create database table"
+echo "create database and user"
 mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MARIA_DB_NAME}\`;"
-
-echo "create user"
 mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';"
-
-echo "grant privileges to the user"
 mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MARIA_DB_NAME}\`.* TO '${MARIA_USER}'@'%';"
-
-echo "flush privileges"
 mariadb -u root -p"${MARIA_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
-echo "shutdown the server"
+echo "shutdown for clean restart"
 mariadb-admin -u root -p"${MARIA_ROOT_PASSWORD}" shutdown
+
+touch "$INIT_MARKER"
+else
+    echo "database already exist"
+fi
 
 echo "starting mariadb"
 exec "$@"
